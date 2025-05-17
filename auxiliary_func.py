07faeaf3353109c0d1374c21ffd1aa4fba485ad2
KARTIK 2ND <Kartik2nd@KARTIKs-MacBook-Air.local>
2025-05-17 14:20:41 +0530

    Modified auxiliary_func.py

--- auxiliary_func.py
+++ auxiliary_func.py
@@ -3,21 +3,15 @@ from chess import Board
 
 
 def board_to_matrix(board: Board):
-    # 8x8 is a size of the chess board.
-    # 12 = number of unique pieces.
-    # 13th board for legal moves (WHERE we can move)
-    # maybe 14th for squares FROM WHICH we can move? idk
     matrix = np.zeros((13, 8, 8))
     piece_map = board.piece_map()
 
-    # Populate first 12 8x8 boards (where pieces are)
     for square, piece in piece_map.items():
         row, col = divmod(square, 8)
         piece_type = piece.piece_type - 1
         piece_color = 0 if piece.color else 6
         matrix[piece_type + piece_color, row, col] = 1
 
-    # Populate the legal moves board (13th 8x8 board)
     legal_moves = board.legal_moves
     for move in legal_moves:
         to_square = move.to_square
